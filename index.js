3d49f8ad5b43d0d79b6bd736bdf641b00e759f09
Justin Tooley <jstooley823@gmail.com>
2018-02-17 03:32:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function lowerCaseDrivers(drivers){
 };
 
 function nameToAttributes(drivers){
-  const splitDrivers = drivers.map(driver =>);
+  splitDrivers = drivers.map(driver =>);
 };
 
 function attributesToPhrase(){};
